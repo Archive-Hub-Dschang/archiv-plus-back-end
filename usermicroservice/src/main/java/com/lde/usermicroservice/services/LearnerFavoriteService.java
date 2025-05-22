736e13a2ca35e2d71f986fe2531d6c699b059abb
Carnelle Enga <carnelleenga@gmail.com>
2025-05-22 12:47:46 +0100

    feat: creation du service(logique metier )   pour la gestion des favorits  d'un utilisateur

---
+++ usermicroservice/src/main/java/com/lde/usermicroservice/services/LearnerFavoriteService.java
@@ -0,0 +1,38 @@
+package com.lde.usermicroservice.services;
+
+import com.lde.usermicroservice.models.LearnerFavorite;
+import com.lde.usermicroservice.repositories.LearnerFavoriteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class LearnerFavoriteService {
+    @Autowired
+    private LearnerFavoriteRepository favoriteRepository;
+//methode pour ajouter un favoris
+    public LearnerFavorite addFavorite(Long learnerId, String subjectId) {
+        Optional<LearnerFavorite> existing = favoriteRepository.findByLearnerIdAndSubjectId(learnerId, subjectId);
+        if (existing.isPresent()) {
+            throw new IllegalStateException("Sujet déjà ajouté aux favoris");
+        }
+        LearnerFavorite favorite = new LearnerFavorite();
+        favorite.setLearnerId(learnerId);
+        favorite.setSubjectId(subjectId);
+        favorite.setAddedAt(LocalDateTime.now());
+
+        return favoriteRepository.save(favorite);
+    }
+//    methode pour avoir tous les favorits d'un utlisateur
+public List<LearnerFavorite> getFavorites(Long learnerId) {
+    return favoriteRepository.findByLearnerId(learnerId);
+}
+//methode pour suprimmer un sujet des favoris
+    public void removeFavorite(Long learnerId, String subjectId) {
+        favoriteRepository.findByLearnerIdAndSubjectId(learnerId, subjectId)
+                .ifPresent(favoriteRepository::delete);
+    }
+}
