23b8f1a8cefc30be75d356f97888e47352b5bc88
sorheil <sorheiltchokothe@gmail.com>
2025-05-23 07:52:47 +0100

    ajout de la validation sur les donnée des formulaire de login et d'enregistrement

--- usermicroservice/src/main/java/com/lde/usermicroservice/dto/LoginUserRequestDTO.java
+++ usermicroservice/src/main/java/com/lde/usermicroservice/dto/LoginUserRequestDTO.java
@@ -1,11 +1,19 @@
 package com.lde.usermicroservice.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class LoginUserRequestDTO {
+    @NotBlank(message = "L'email est requis.")
+    @Email(message = "L'email doit être valide.")
     private String email;
+
+    @NotBlank(message = "Le mot de passe est requis.")
+    @Size(min = 6, message = "Le mot de passe doit contenir au moins 6 caractères.")
     private String password;
 }
