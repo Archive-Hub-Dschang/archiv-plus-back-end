67161823be2ea52ca53a16449b786cdf0967e3c3
Carnelle Enga <carnelleenga@gmail.com>
2025-06-02 01:33:06 +0100

    fix:modification de document en examen pour l'ajout aux favoris

--- usermicroservice/src/main/java/com/lde/usermicroservice/models/User.java
+++ usermicroservice/src/main/java/com/lde/usermicroservice/models/User.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 @Entity(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(nullable = false, unique = true)
